6b97e30d50a6c5e023f7df1cec49e95324b4070a
qarr <thecoder.co@gmail.com>
2022-10-11 16:25:33 +0100

    ...

---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers togetehr"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers togetehr"""
+        res = calc.subtract(10, 5)
+
+        self.assertEqual(res, 5)
