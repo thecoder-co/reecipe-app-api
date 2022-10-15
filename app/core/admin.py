6c9ee9d0837ff17ef16090deb53edd5c5c4b47b0
thecoder-co <thecoder.co@gmail.com>
2022-10-15 17:06:32 +0100

    ...

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,2 +1,32 @@
+"""
+Django admin customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', )})
+    )
+
+
+admin.site.register(models.User, UserAdmin)
